3620f615e828f3fe8da26443bd95a1de49f6c652
jeesaas <jeesaas@163.com>
2022-01-29 22:16:09 +0800

    first commit

--- src/main.js
+++ src/main.js
@@ -4,8 +4,10 @@ import App from './App.vue'
 import router from './router'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import request from './utils/request'
 
 const app = createApp(App)
+app.config.globalProperties.$request = request;
 app.use(ElementPlus)
 // 加载路由信息
 app.use(router)
